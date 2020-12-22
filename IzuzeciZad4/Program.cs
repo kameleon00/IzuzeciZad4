2be9267cefbc6278cf6643fb1b1827bbf8fefa58
Marko Djokic <marko1319@its.edu.rs>
2020-12-22 16:00:25 +0100

    Add project files.

---
+++ IzuzeciZad4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IzuzeciZad4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int imenilac;
+            bool imenilacOK =true;
+
+            do
+            {
+                Console.Write("Unesite imenilac: ");
+                imenilacOK = Int32.TryParse(Console.ReadLine(), out imenilac);
+            }
+            while (!imenilacOK || imenilac==0);
+            Console.Write("Imenilac: {0}", imenilac);
+            Console.WriteLine();
+            //-------------------------------------------------------------------------
+            //-------------------------------------------------------------------------
+
+            int brojilac;
+            bool brojilacOK = true;
+
+            do
+            {
+                Console.Write("Unesite brojilac: ");
+                brojilacOK = Int32.TryParse(Console.ReadLine(), out brojilac);
+            }
+            while (!brojilacOK);
+            Console.Write("Brojilac: {0}", brojilac);
+            Console.WriteLine();
+            //-------------------------------------------------------------------------
+            //-------------------------------------------------------------------------
+
+            Console.Write("Rezultat deljenja je: {0}", (float)brojilac / imenilac);
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
